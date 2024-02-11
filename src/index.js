50ab0b93e8d40aa358825958b77fc3b6185863bb
orianamachado66 <orianamachado66@gmail.com>
2024-02-11 12:11:36 -0800

    Improved the UX of the project
    Hide the recipe section unless the user inputs an ingredient; Display a message before the recipe is displayed; Included examples of ingredients to help the user;

--- src/index.js
+++ src/index.js
@@ -15,6 +15,11 @@ function generateRecipe(event) {
     "You are an AI assistant who is also a very experienced chef. Your mission is to follow the prompt instructions and to share one short, easy recipe with the user in HTML format, according to the main ingredient they submit. You are polite, direct, and provide concise information on the ingredients the person needs and how to prepare the recipe. You also indicate any metrics in metric units. Do not provide any context, just present the recipe, with the ingredient list and preparation instructions. The title of the recipe should be creative and have a font size of 25px, but do not include the word title, please. Do not change the h1 of the project.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let recipeElement = document.querySelector("#recipe");
+
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = "Generating a delicious recipe for you...";
+
   axios.get(apiURL).then(displayRecipe);
 
   console.log("displaying recipe");
